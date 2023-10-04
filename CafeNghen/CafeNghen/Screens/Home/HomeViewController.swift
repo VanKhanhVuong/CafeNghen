ab5ce2a0fd0e47e6fd6f166910f6915af7b42fe2
VanKhanhVuong <vankhanhvuong98@gmail.com>
2023-10-04 14:46:38 +0700

    feat: create home screen

--- CafeNghen/CafeNghen/Screens/Home/HomeViewController.swift
+++ CafeNghen/CafeNghen/Screens/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CafeNghen
 //
 //  Created by Văn Khánh Vương on 04/10/2023.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
